e559e65b323cd27d79f98bcf240612e199da7916
robmikh <rob.mikh@outlook.com>
2021-02-19 00:01:06 -0800

    add comment

--- CompositionImageDemo/main.cpp
+++ CompositionImageDemo/main.cpp
@@ -13,146 +13,26 @@ namespace winrt
     using namespace Windows::UI::Composition;
 }
 
+// This sample uses some common helper functions. They'll be noted with a link to the source.
 namespace util
 {
     using namespace robmikh::common::uwp;
     using namespace robmikh::common::desktop;
 }
 
-// We can only use IAsync* with WinRT objects
-std::future<winrt::com_ptr<ID3D11Texture2D>> CreateTextureFromImageAsync(winrt::com_ptr<ID3D11Device> const& d3dDevice)
-{
-    // Get our own references for the coroutine
-    auto device = d3dDevice;
-
-    // You'll need to get a stream to your file. This demo uses a local image.
-    auto currentPath = std::filesystem::current_path();
-    auto folder = co_await winrt::StorageFolder::GetFolderFromPathAsync(currentPath.wstring());
-    auto file = co_await folder.GetFileAsync(L"tripphoto1.jpg");
-
-    winrt::com_ptr<ID3D11Texture2D> texture;
-    {
-        auto stream = co_await file.OpenReadAsync();
-
-        // Create the decoder for our image
-        auto decoder = co_await winrt::BitmapDecoder::CreateAsync(stream);
-        // Since this image is a jpg it only has a single frame
-        auto frame = co_await decoder.GetFrameAsync(0);
-        auto width = frame.PixelWidth();
-        auto height = frame.PixelHeight();
-        WINRT_ASSERT(frame.BitmapPixelFormat() == winrt::BitmapPixelFormat::Bgra8);
-
-        auto pixelData = co_await frame.GetPixelDataAsync();
-        auto bytes = pixelData.DetachPixelData();
-
-        // Now we need to create a D3D Texture
-        D3D11_TEXTURE2D_DESC desc = {};
-        desc.Width = width;
-        desc.Height = height;
-        desc.MipLevels = 1;
-        desc.ArraySize = 1;
-        desc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
-        desc.SampleDesc.Count = 1;
-
-        D3D11_SUBRESOURCE_DATA initData = {};
-        initData.pSysMem = bytes.data();
-        // This describes how many bytes wide our image is.
-        // Each BGRA pixel is 4 bytes.
-        initData.SysMemPitch = width * 4;
-
-        winrt::check_hresult(device->CreateTexture2D(&desc, &initData, texture.put()));
-    }
-    co_return texture;
-}
-
+// We can only use IAsyncOperation with WinRT objects
+std::future<winrt::com_ptr<ID3D11Texture2D>> CreateTextureFromImageAsync(winrt::com_ptr<ID3D11Device> const& d3dDevice);
 void CopyTexutreIntoCompositionSurface(
     winrt::CompositionDrawingSurface const& surface,
     winrt::com_ptr<ID3D11Texture2D> const& sourceTexture,
-    winrt::com_ptr<ID3D11DeviceContext> const& d3dContext)
-{
-    // Since we're going to interop with D3D, we'll need the inteorp COM interface from the surface.
-    auto surfaceInterop = surface.as<ABI::Windows::UI::Composition::ICompositionDrawingSurfaceInterop>();
-
-    // Make sure our surface is the correct size for our image.
-    D3D11_TEXTURE2D_DESC desc = {};
-    sourceTexture->GetDesc(&desc);
-    winrt::check_hresult(surfaceInterop->Resize({ static_cast<LONG>(desc.Width), static_cast<LONG>(desc.Height) }));
-
-    // Here we get the underlying D3D texture for our surface. Because composition surfaces come from an
-    // atlas, we need to copy our data at an offset. 
-    POINT offset = {};
-    winrt::com_ptr<ID3D11Texture2D> surfaceTexture;
-    winrt::check_hresult(surfaceInterop->BeginDraw(nullptr, winrt::guid_of<ID3D11Texture2D>(), surfaceTexture.put_void(), &offset));
-    // Make sure that you call EndDraw when you're finished.
-    auto scopeExit = wil::scope_exit([surfaceInterop]()
-        {
-            winrt::check_hresult(surfaceInterop->EndDraw());
-        });
-
-    d3dContext->CopySubresourceRegion(surfaceTexture.get(), 0, offset.x, offset.y, 0, sourceTexture.get(), 0, nullptr);
-}
-
+    winrt::com_ptr<ID3D11DeviceContext> const& d3dContext);
 winrt::fire_and_forget LoadImageIntoSurface(
-    winrt::CompositionDrawingSurface const& surface, 
-    winrt::com_ptr<ID3D11Device> const& d3dDevice)
-{
-    winrt::com_ptr<ID3D11DeviceContext> d3dContext;
-    d3dDevice->GetImmediateContext(d3dContext.put());
-
-    auto imageTexture = co_await CreateTextureFromImageAsync(d3dDevice);
-
-    CopyTexutreIntoCompositionSurface(surface, imageTexture, d3dContext);
-    co_return;
-}
-
+    winrt::CompositionDrawingSurface const& surface,
+    winrt::com_ptr<ID3D11Device> const& d3dDevice);
 winrt::fire_and_forget RegisterForDeviceLost(
     wil::shared_event const& eventHandle,
     winrt::com_ptr<ID3D11Device> const& d3dDevice,
-    winrt::CompositionGraphicsDevice const& compositionGraphics)
-{
-    // Get our own references for the coroutine
-    auto deviceLostEvent = eventHandle;
-    auto compGraphics = compositionGraphics;
-
-    DWORD cookie = 0;
-    auto d3dDevice4 = d3dDevice.as<ID3D11Device4>();
-    winrt::check_hresult(d3dDevice4->RegisterDeviceRemovedEvent(deviceLostEvent.get(), &cookie));
-
-    co_await winrt::resume_on_signal(deviceLostEvent.get());
-    deviceLostEvent.ResetEvent(); // Reset the event since we're reusing it.
-    d3dDevice4->UnregisterDeviceRemoved(cookie);
-
-    while (true)
-    {
-        try
-        {
-            // Create a new D3D device and tell our CompositionGraphicsDevice about it
-            auto newD3dDevice = util::CreateD3DDevice();
-            RegisterForDeviceLost(deviceLostEvent, newD3dDevice, compGraphics);
-
-            // This will cause the RenderingDeviceReplaced event to fire on the CompositionGraphicsDevice
-            auto graphicsDeviceInterop = compGraphics.as<ABI::Windows::UI::Composition::ICompositionGraphicsDeviceInterop>();
-            winrt::check_hresult(graphicsDeviceInterop->SetRenderingDevice(newD3dDevice.get()));
-
-            break;
-        }
-        catch (winrt::hresult_error const& error)
-        {
-            auto errorCode = error.code();
-            if (errorCode == DXGI_ERROR_DEVICE_REMOVED ||
-                errorCode == DXGI_ERROR_DEVICE_RESET)
-            {
-                // Loop around to try again.
-            }
-            else
-            {
-                throw;
-            }
-        }
-        // Don't try again too soon.
-        co_await std::chrono::milliseconds(500);
-    }
-}
+    winrt::CompositionGraphicsDevice const& compositionGraphics);
 
 int __stdcall WinMain(HINSTANCE, HINSTANCE, PSTR, int)
 {
@@ -238,4 +118,139 @@ int __stdcall WinMain(HINSTANCE, HINSTANCE, PSTR, int)
     }
 
     return static_cast<int>(msg.wParam);
+}
+
+// We can only use IAsync* with WinRT objects
+std::future<winrt::com_ptr<ID3D11Texture2D>> CreateTextureFromImageAsync(winrt::com_ptr<ID3D11Device> const& d3dDevice)
+{
+    // Get our own references for the coroutine
+    auto device = d3dDevice;
+
+    // You'll need to get a stream to your file. This demo uses a local image.
+    auto currentPath = std::filesystem::current_path();
+    auto folder = co_await winrt::StorageFolder::GetFolderFromPathAsync(currentPath.wstring());
+    auto file = co_await folder.GetFileAsync(L"tripphoto1.jpg");
+
+    winrt::com_ptr<ID3D11Texture2D> texture;
+    {
+        auto stream = co_await file.OpenReadAsync();
+
+        // Create the decoder for our image
+        auto decoder = co_await winrt::BitmapDecoder::CreateAsync(stream);
+        // Since this image is a jpg it only has a single frame
+        auto frame = co_await decoder.GetFrameAsync(0);
+        auto width = frame.PixelWidth();
+        auto height = frame.PixelHeight();
+        WINRT_ASSERT(frame.BitmapPixelFormat() == winrt::BitmapPixelFormat::Bgra8);
+
+        auto pixelData = co_await frame.GetPixelDataAsync();
+        auto bytes = pixelData.DetachPixelData();
+
+        // Now we need to create a D3D Texture
+        D3D11_TEXTURE2D_DESC desc = {};
+        desc.Width = width;
+        desc.Height = height;
+        desc.MipLevels = 1;
+        desc.ArraySize = 1;
+        desc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
+        desc.SampleDesc.Count = 1;
+
+        D3D11_SUBRESOURCE_DATA initData = {};
+        initData.pSysMem = bytes.data();
+        // This describes how many bytes wide our image is.
+        // Each BGRA pixel is 4 bytes.
+        initData.SysMemPitch = width * 4;
+
+        winrt::check_hresult(device->CreateTexture2D(&desc, &initData, texture.put()));
+    }
+    co_return texture;
+}
+
+void CopyTexutreIntoCompositionSurface(
+    winrt::CompositionDrawingSurface const& surface,
+    winrt::com_ptr<ID3D11Texture2D> const& sourceTexture,
+    winrt::com_ptr<ID3D11DeviceContext> const& d3dContext)
+{
+    // Since we're going to interop with D3D, we'll need the inteorp COM interface from the surface.
+    auto surfaceInterop = surface.as<ABI::Windows::UI::Composition::ICompositionDrawingSurfaceInterop>();
+
+    // Make sure our surface is the correct size for our image.
+    D3D11_TEXTURE2D_DESC desc = {};
+    sourceTexture->GetDesc(&desc);
+    winrt::check_hresult(surfaceInterop->Resize({ static_cast<LONG>(desc.Width), static_cast<LONG>(desc.Height) }));
+
+    // Here we get the underlying D3D texture for our surface. Because composition surfaces come from an
+    // atlas, we need to copy our data at an offset. 
+    POINT offset = {};
+    winrt::com_ptr<ID3D11Texture2D> surfaceTexture;
+    winrt::check_hresult(surfaceInterop->BeginDraw(nullptr, winrt::guid_of<ID3D11Texture2D>(), surfaceTexture.put_void(), &offset));
+    // Make sure that you call EndDraw when you're finished.
+    auto scopeExit = wil::scope_exit([surfaceInterop]()
+        {
+            winrt::check_hresult(surfaceInterop->EndDraw());
+        });
+
+    d3dContext->CopySubresourceRegion(surfaceTexture.get(), 0, offset.x, offset.y, 0, sourceTexture.get(), 0, nullptr);
+}
+
+winrt::fire_and_forget LoadImageIntoSurface(
+    winrt::CompositionDrawingSurface const& surface,
+    winrt::com_ptr<ID3D11Device> const& d3dDevice)
+{
+    winrt::com_ptr<ID3D11DeviceContext> d3dContext;
+    d3dDevice->GetImmediateContext(d3dContext.put());
+
+    auto imageTexture = co_await CreateTextureFromImageAsync(d3dDevice);
+
+    CopyTexutreIntoCompositionSurface(surface, imageTexture, d3dContext);
+    co_return;
+}
+
+winrt::fire_and_forget RegisterForDeviceLost(
+    wil::shared_event const& eventHandle,
+    winrt::com_ptr<ID3D11Device> const& d3dDevice,
+    winrt::CompositionGraphicsDevice const& compositionGraphics)
+{
+    // Get our own references for the coroutine
+    auto deviceLostEvent = eventHandle;
+    auto compGraphics = compositionGraphics;
+
+    DWORD cookie = 0;
+    auto d3dDevice4 = d3dDevice.as<ID3D11Device4>();
+    winrt::check_hresult(d3dDevice4->RegisterDeviceRemovedEvent(deviceLostEvent.get(), &cookie));
+
+    co_await winrt::resume_on_signal(deviceLostEvent.get());
+    deviceLostEvent.ResetEvent(); // Reset the event since we're reusing it.
+    d3dDevice4->UnregisterDeviceRemoved(cookie);
+
+    while (true)
+    {
+        try
+        {
+            // Create a new D3D device and tell our CompositionGraphicsDevice about it
+            auto newD3dDevice = util::CreateD3DDevice();
+            RegisterForDeviceLost(deviceLostEvent, newD3dDevice, compGraphics);
+
+            // This will cause the RenderingDeviceReplaced event to fire on the CompositionGraphicsDevice
+            auto graphicsDeviceInterop = compGraphics.as<ABI::Windows::UI::Composition::ICompositionGraphicsDeviceInterop>();
+            winrt::check_hresult(graphicsDeviceInterop->SetRenderingDevice(newD3dDevice.get()));
+
+            break;
+        }
+        catch (winrt::hresult_error const& error)
+        {
+            auto errorCode = error.code();
+            if (errorCode == DXGI_ERROR_DEVICE_REMOVED ||
+                errorCode == DXGI_ERROR_DEVICE_RESET)
+            {
+                // Loop around to try again.
+            }
+            else
+            {
+                throw;
+            }
+        }
+        // Don't try again too soon.
+        co_await std::chrono::milliseconds(500);
+    }
 }
